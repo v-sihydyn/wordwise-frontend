ff979fff7148dde6eb899c232461a1019f40207a
Vitalii Sihydyn <v.sihydyn@gmail.com>
2024-09-06 12:38:11 +0300

    home page markup

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,231 +1,27 @@
 import { BasePageTemplate } from '@/templates/BasePageTemplate';
 import { Header } from '@/components/Header/Header';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <BasePageTemplate header={<Header />}>
-      <div className="flex flex-1 justify-center px-40 py-5">
-        <div className="layout-content-container flex max-w-[960px] flex-1 flex-col">
-          <div className="flex flex-wrap gap-3 p-3 pr-4">
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">A</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">B</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">C</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">D</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">E</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">F</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">G</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">H</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">I</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">J</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">K</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">L</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">M</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">N</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">O</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">P</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">Q</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">R</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">S</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">T</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">U</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">V</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">W</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">X</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">Y</p>
-            </div>
-            <div className="flex h-8 shrink-0 items-center justify-center gap-x-2 rounded-xl bg-[#283139] pl-4 pr-4">
-              <p className="text-sm font-medium leading-normal text-white">Z</p>
-            </div>
-          </div>
-          <h2 className="px-4 pb-3 pt-5 text-[22px] font-bold leading-tight tracking-[-0.015em] text-white">
-            Recently Searched
-          </h2>
-          <div className="flex min-h-[72px] items-center justify-between gap-4 bg-[#111518] px-4 py-2">
-            <div className="flex items-center gap-4">
-              <div
-                className="flex size-12 shrink-0 items-center justify-center rounded-lg bg-[#283139] text-white"
-                data-icon="MagnifyingGlass"
-                data-size="24px"
-                data-weight="regular"
+      <div className="container mx-auto flex flex-1 justify-center px-40 py-5">
+        <div className="layout-content-container flex flex-1 flex-col">
+          <div className="grid grid-cols-5 gap-3 p-3">
+            {alphabet.map((l) => (
+              <Link
+                key={l}
+                href="/words"
+                className="flex shrink-0 items-center justify-center rounded-xl bg-[#283139] p-6 pl-4 pr-4"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24px"
-                  height="24px"
-                  fill="currentColor"
-                  viewBox="0 0 256 256"
-                >
-                  <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
-                </svg>
-              </div>
-              <div className="flex flex-col justify-center">
-                <p className="line-clamp-1 text-base font-medium leading-normal text-white">shiny</p>
-                <p className="line-clamp-2 text-sm font-normal leading-normal text-[#9cacba]">adjective</p>
-              </div>
-            </div>
-            <div className="shrink-0">
-              <p className="text-base font-normal leading-normal text-white">adjective</p>
-            </div>
-          </div>
-          <div className="flex min-h-[72px] items-center justify-between gap-4 bg-[#111518] px-4 py-2">
-            <div className="flex items-center gap-4">
-              <div
-                className="flex size-12 shrink-0 items-center justify-center rounded-lg bg-[#283139] text-white"
-                data-icon="MagnifyingGlass"
-                data-size="24px"
-                data-weight="regular"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24px"
-                  height="24px"
-                  fill="currentColor"
-                  viewBox="0 0 256 256"
-                >
-                  <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
-                </svg>
-              </div>
-              <div className="flex flex-col justify-center">
-                <p className="line-clamp-1 text-base font-medium leading-normal text-white">car</p>
-                <p className="line-clamp-2 text-sm font-normal leading-normal text-[#9cacba]">noun</p>
-              </div>
-            </div>
-            <div className="shrink-0">
-              <p className="text-base font-normal leading-normal text-white">noun</p>
-            </div>
-          </div>
-          <div className="flex min-h-[72px] items-center justify-between gap-4 bg-[#111518] px-4 py-2">
-            <div className="flex items-center gap-4">
-              <div
-                className="flex size-12 shrink-0 items-center justify-center rounded-lg bg-[#283139] text-white"
-                data-icon="MagnifyingGlass"
-                data-size="24px"
-                data-weight="regular"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24px"
-                  height="24px"
-                  fill="currentColor"
-                  viewBox="0 0 256 256"
-                >
-                  <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
-                </svg>
-              </div>
-              <div className="flex flex-col justify-center">
-                <p className="line-clamp-1 text-base font-medium leading-normal text-white">moon</p>
-                <p className="line-clamp-2 text-sm font-normal leading-normal text-[#9cacba]">noun</p>
-              </div>
-            </div>
-            <div className="shrink-0">
-              <p className="text-base font-normal leading-normal text-white">noun</p>
-            </div>
-          </div>
-          <div className="flex min-h-[72px] items-center justify-between gap-4 bg-[#111518] px-4 py-2">
-            <div className="flex items-center gap-4">
-              <div
-                className="flex size-12 shrink-0 items-center justify-center rounded-lg bg-[#283139] text-white"
-                data-icon="MagnifyingGlass"
-                data-size="24px"
-                data-weight="regular"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24px"
-                  height="24px"
-                  fill="currentColor"
-                  viewBox="0 0 256 256"
-                >
-                  <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
-                </svg>
-              </div>
-              <div className="flex flex-col justify-center">
-                <p className="line-clamp-1 text-base font-medium leading-normal text-white">fast</p>
-                <p className="line-clamp-2 text-sm font-normal leading-normal text-[#9cacba]">adjective</p>
-              </div>
-            </div>
-            <div className="shrink-0">
-              <p className="text-base font-normal leading-normal text-white">adjective</p>
-            </div>
-          </div>
-          <div className="flex min-h-[72px] items-center justify-between gap-4 bg-[#111518] px-4 py-2">
-            <div className="flex items-center gap-4">
-              <div
-                className="flex size-12 shrink-0 items-center justify-center rounded-lg bg-[#283139] text-white"
-                data-icon="MagnifyingGlass"
-                data-size="24px"
-                data-weight="regular"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24px"
-                  height="24px"
-                  fill="currentColor"
-                  viewBox="0 0 256 256"
-                >
-                  <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z"></path>
-                </svg>
-              </div>
-              <div className="flex flex-col justify-center">
-                <p className="line-clamp-1 text-base font-medium leading-normal text-white">blue</p>
-                <p className="line-clamp-2 text-sm font-normal leading-normal text-[#9cacba]">adjective</p>
-              </div>
-            </div>
-            <div className="shrink-0">
-              <p className="text-base font-normal leading-normal text-white">adjective</p>
-            </div>
+                <p className="text-lg font-medium leading-normal text-white">{l}</p>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
     </BasePageTemplate>
   );
 }
+
+const alphabet = Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i));
